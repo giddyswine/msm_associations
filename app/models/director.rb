c84f4d2302d8b967166a3d105a431946822c2e73
Grant Ziesemer <gzieseme@chicagobooth.edu>
2017-08-02 20:19:53 +0000

    actors uploaded

--- app/models/director.rb
+++ app/models/director.rb
@@ -4,6 +4,14 @@ class Director < ApplicationRecord
 # Table name: directors
 #
 #  id         :integer          not null, primary key
+#  name       :string
+#  dob        :string
+#  bio        :text
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 validates :name, :dob, :url, presence: true
 validates :bio, integer: false, presence: true
 #  name       :string
